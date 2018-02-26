932b13122f258d8cfcffa777bfae59d31b1b7246
Gergely Papp <gergely.papp@student.manchester.ac.uk>
2018-02-26 22:26:26 +0100

    config prepared for gomoku3d

--- src/games/gomoku3d/config.py
+++ src/games/gomoku3d/config.py
@@ -14,8 +14,8 @@ CONNECT_SIZE = BOARD_SIZE
 
 NEURAL_NET_SETTINGS = {
     'no_of_possible_actions' : BOARD_SIZE * BOARD_SIZE,
-    'filter_n' : 128,
-    'res_layer_n' : 2,
+    'filter_n' : 160,
+    'res_layer_n' : 5,
     'input_shape' : (BOARD_SIZE*2, BOARD_SIZE, BOARD_SIZE),
 }
 
