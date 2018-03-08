d11b60670c7a93b2240b2e3676de36891c9036b1
Gergely Papp <gergely.papp@student.manchester.ac.uk>
2018-03-08 22:34:42 +0100

    prepared for gomoku3d train

--- src/config.py
+++ src/config.py
@@ -7,12 +7,12 @@ WORK_FOLDER = 'bin/'
 
 DEFAULT_NEURAL_NET_SETTINGS = {
     'no_of_possible_actions' : None,      # The number of actions the softmax layer produces
-    'value_hidden_size' : 128,             # Size of hidden layer in value head 
-    'res_layer_n' : 2,                    # The number of residual layers
-    'filter_n' : 32,                     # The number of filters in a conv layer
+    'value_hidden_size' : 256,             # Size of hidden layer in value head 
+    'res_layer_n' : 4,                    # The number of residual layers
+    'filter_n' : 160,                     # The number of filters in a conv layer
     'kernel_size' : 3,
     'batch_size' : 100,                    
-    'epochs' : 3,
+    'epochs' : 2,
     'verbose' : 0,
     'validation_split' : 0.05,
     'input_shape' : None,
@@ -30,12 +30,12 @@ MINIMUM_TEMPERATURE_ACCEPTED = 0.2
 
 # How many seconds to think on a move while training
 DEFAULT_TRAIN_THINK_TIME = 1.2
-DEFAULT_NUMBER_OF_SIMULATIONS = 100
+DEFAULT_NUMBER_OF_SIMULATIONS = 150
 
 # The maximum number of positions to save
-MEMORY_SIZE = 40000
+MEMORY_SIZE = 100000
 
-NO_OF_EPISODES = 200
+NO_OF_EPISODES = 150
 NO_OF_ITERATIONS = 50
 NO_OF_GAMES_TO_BATTLE = 20
 SAVE_THRESHOLD = 0.5501
